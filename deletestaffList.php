34f092184a9099adba31b4d44b176e5d42f27603
zuhdi04 <zuhdimuhd10@gmail.com>
2025-01-14 16:53:52 +0800

    delete admin account

---
+++ deletestaffList.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-Type: application/json');
+
+// Create a connection
+$conn = new mysqli('localhost', 'root', '', 'zuhdiscmsdb');
+
+// Check connection
+if ($conn->connect_error) {
+    echo json_encode(['error' => 'Connection failed: ' . $conn->connect_error]);
+    exit;
+}
+
+$target = $_POST['target'];
+$stmt = $conn->prepare("DELETE FROM admin WHERE username=?");
+$stmt ->bind_param("s", $target);
+if ($stmt->execute()) {
+    echo json_encode(['message' => 'Update successful']);
+} else {
+    echo json_encode(['message' => 'Update failed: ' . $stmt->error]);
+}
+$stmt->close();
+$conn->close();
+?>
--- js/dashboard.js
+++ js/dashboard.js
@@ -6,12 +6,40 @@ function details(){
         response.forEach(item => {
             $('#stafflist').append(`
                 <tr>
-                    <td id="pc_`+item.id+`">`+item.username+`</td>
+                    <td>`+item.username+`</td>
                     <td>`+item.cawangan+`</td>
-                    <td><a href="staff-edit.php?form=`+item.id+`" onclick="return false">Edit</a><a href="#" class="delete_details" data-id="`+item.id+`" onclick="return false;">Delete</a></td>
+                    <td><a href="staff-edit.php?form=`+item.id+`" onclick="return false">Edit</a>
+                    <a href="delete" class="delete_details" data-name="`+item.username+`" onclick="return false;">Delete</a></td>
                 </tr>`);
         });
     }
     xhttp.open("GET", "getstaffList.php", true);
     xhttp.send();
 }
+
+$('#staffTable').on('click','.delete_details',function(){
+    let n = $(this).data('name');
+    
+    if(confirm("Adakah anda mahu memadam akaun: "+n+"?")){
+        // Perform AJAX request
+        $.ajax({
+            url: 'deletestaffList.php', // Replace with your API endpoint
+            method: 'POST', // Use POST for updates
+            data: {
+                target: n
+            },
+            success: function(response) {
+                // Handle successful response
+                console.log('Update successful:', response);
+                alert('Akaun telah dipadam!');
+                details();
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                // Handle error
+                console.error('Update failed:', textStatus, errorThrown);
+                alert('Error updating data.');
+            }
+        });
+    }
+    details();
+});
